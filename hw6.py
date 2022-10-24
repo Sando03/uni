63370272192fb50c8ed6464866f2253a9c5a877b
Sando03 <115527011+Sando03@users.noreply.github.com>
2022-10-24 09:33:04 +0300

    Update hw6.py

--- hw6.py
+++ hw6.py
@@ -2,7 +2,7 @@ list1=[1,2,3,4,5]
 list2=[]
 for i in range(len(list1)-1):
     sum= list1[i]+list1[i+1]
-    list2.append(list1[i+0])
+    list2.append(list1[i])
     list2.append(sum)
     sum=0
 list2.append(list1[4])
